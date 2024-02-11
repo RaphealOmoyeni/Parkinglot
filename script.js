4f37dc737ccb064de3602b3576373be91a637232
RaphealOmoyeni <metrogroupconcept@gmail.com>
2024-02-11 23:57:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"Strict";
+
+class SlotCl {
+  slots = [];
+  constructor(parkingSize) {
+    const level1 = new Array(parkingSize).fill(null);
+    const level2 = new Array(parkingSize).fill(null);
+    const level3 = new Array(parkingSize).fill(null);
+
+    this.slots.push(level1, level2, level3);
+    console.log(this.slots[1]);
+  }
+}
+
+const levelOne = new SlotCl(10);
